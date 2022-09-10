241bad603b0d11366cf1ec794984456833a2c041
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2022-09-10 20:08:48 +0200

    Add eslint to web dir

---
+++ web/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:astro/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  ],
+};
