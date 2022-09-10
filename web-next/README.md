1f0f606f83bc2b987196e5961fdf2ec283acc1e7
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2022-09-10 21:19:53 +0200

    Remove next app and update workflows

--- web-next/README.md
+++
@@ -1,1 +0,0 @@
-# Kodio web
--- web-next/lib/sanityConfig.ts
+++
@@ -1,6 +0,0 @@
-export const sanityConfig = {
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  useCdn: true,
-  apiVersion: '2022-09-06',
-};
