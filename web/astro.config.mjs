63ecbc81aa25899a084be888e122554c264f2f5b
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2022-09-13 21:21:37 +0200

    Add sitemap

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,7 +1,9 @@
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Glitner.github.io',
   base: '/kodio-web',
+  integrations: [sitemap()],
 });
