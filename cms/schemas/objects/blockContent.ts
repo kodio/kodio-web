a0d3ad0ceb664dff1144d278597f323ea3a463e9
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2022-09-13 16:27:36 +0200

    Rename cms directory

--- cms/schemas/objects/blockContent.ts
+++ cms/schemas/objects/blockContent.ts
@@ -2,6 +2,7 @@ import { defineType } from 'sanity';
 
 export const blockContent = defineType({
   name: 'blockContent',
+  title: 'Block content',
   type: 'array',
   of: [{ type: 'block' }],
 });
