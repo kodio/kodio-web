5a13e777dac68a6c4edca060f0da8a2d956f14d7
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2022-09-21 16:43:53 +0200

    Add BlockContent component and add kodio colors

--- cms/sanity.config.tsx
+++ cms/sanity.config.tsx
@@ -5,6 +5,7 @@ import { schemaTypes } from './schemas';
 export default createConfig({
   name: 'default',
   title: 'kodio-sanity-tmp',
+  logo: () => <>Kodio.</>,
 
   projectId: '7veh4wq9',
   dataset: 'production',
