9283f41439d5c4651bf12b36a745999d15d7c324
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2022-10-15 16:32:02 +0200

    Use async decoding for images, change interface to type and add vscode settings

--- web/src/lib/sanity/types.ts
+++ web/src/lib/sanity/types.ts
@@ -1,47 +1,47 @@
 import type { ArbitraryTypedObject } from '@portabletext/types';
 
-export interface Accordion extends ArbitraryTypedObject {
+export type Accordion = ArbitraryTypedObject & {
   accordionItems?: AccordionItem[];
-}
+};
 
-export interface AccordionItem {
+export type AccordionItem = {
   title?: string;
   body?: ArbitraryTypedObject[];
-}
+};
 
-export interface Article extends ArbitraryTypedObject {
+export type Article = ArbitraryTypedObject & {
   body?: ArbitraryTypedObject[];
-}
+};
 
-export interface Banner extends ArbitraryTypedObject {
+export type Banner = ArbitraryTypedObject & {
   heading?: string;
   body?: ArbitraryTypedObject[];
   image?: ImageObj;
   textPosition?: 'left' | 'right';
   links?: Link[];
   linkColor?: Color;
-}
+};
 
-export interface BannerLink {
+export type BannerLink = {
   title?: string;
   preHeading1?: string;
   preHeading2?: string;
   image?: Image;
   links?: Link[];
-}
+};
 
-export interface BannerLinkGroup extends ArbitraryTypedObject {
+export type BannerLinkGroup = ArbitraryTypedObject & {
   bannerLinks?: BannerLink[];
-}
+};
 
-export interface BlockContentExternalLink {
+export type BlockContentExternalLink = {
   href: string;
   blank: boolean;
-}
+};
 
-export interface BlockContentInternalLink {
+export type BlockContentInternalLink = {
   slug: Slug;
-}
+};
 
 export type Color =
   | 'white'
@@ -61,35 +61,35 @@ export type Color =
   | 'yellow-50'
   | 'transparent';
 
-export interface DefaultMetadata {
+export type DefaultMetadata = {
   description: string;
   image: Image;
-}
+};
 
-export interface ExternalLink {
+export type ExternalLink = {
   _type: 'externalLink';
   blank?: boolean;
   href: string;
   label?: string;
-}
+};
 
-export interface FooterConfig {
+export type FooterConfig = {
   sections: FooterConfigSection[];
-}
+};
 
-export interface FooterConfigSection {
+export type FooterConfigSection = {
   heading?: string;
   links: Link[];
-}
+};
 
-export interface Header {
+export type Header = {
   color: Color;
   heading: string;
   links?: Link[];
   image?: ImageObj;
-}
+};
 
-export interface Image {
+export type Image = {
   asset?: {
     _ref?: string;
   };
@@ -105,54 +105,54 @@ export interface Image {
     x: number;
     y: number;
   };
-}
+};
 
-export interface ImageObj extends Image {
+export type ImageObj = Image & {
   _type: 'imageObj';
   alt?: string;
-}
+};
 
-export interface InternalLink {
+export type InternalLink = {
   _type: 'internalLink';
   slug?: Slug;
   label?: string;
-}
+};
 
 export type Link = InternalLink | ExternalLink;
 
-export interface LandingPage {
+export type LandingPage = {
   metadata?: PageMetadata;
   header: Header;
   sections?: Section[];
-}
+};
 
-export interface MenuConfig {
+export type MenuConfig = {
   links: Link[];
-}
+};
 
-export interface Page {
+export type Page = {
   title: string;
   slug: Slug;
   metadata?: PageMetadata;
   header: Header;
   sections?: Section[];
-}
+};
 
-export interface PageMetadata {
+export type PageMetadata = {
   description?: string;
   image?: Image;
-}
+};
 
-export interface Section {
+export type Section = {
   _type: 'section';
   heading?: string;
   color: Color;
   content?: SectionContent[];
-}
+};
 
 type SectionContent = Accordion | Article | Banner | BannerLinkGroup;
 
-export interface Slug {
+export type Slug = {
   _type: 'slug';
   current: string;
-}
+};
