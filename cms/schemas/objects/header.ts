79a9f638b8dbd22ed97a32a85dccee928f322e9f
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2023-02-02 20:55:35 +0100

    Add color picker component to sanity

--- cms/schemas/objects/header.ts
+++ cms/schemas/objects/header.ts
@@ -11,6 +11,26 @@ export const header = defineType({
       type: 'color',
       initialValue: 'yellow-50',
       validation: (Rule) => Rule.required(),
+      options: {
+        // white, green-800
+        list: [
+          { title: 'White', value: 'white' },
+          { title: 'Green 900', value: 'green-900' },
+          { title: 'Green 800', value: 'green-800' },
+          { title: 'Green 700', value: 'green-700' },
+          { title: 'Orange 500', value: 'orange-500' },
+          { title: 'Orange 400', value: 'orange-400' },
+          { title: 'Orange 300', value: 'orange-300' },
+          { title: 'Orange 200', value: 'orange-200' },
+          { title: 'Purple 400', value: 'purple-400' },
+          { title: 'Purple 300', value: 'purple-300' },
+          { title: 'Purple 200', value: 'purple-200' },
+          { title: 'Purple 100', value: 'purple-100' },
+          { title: 'Yellow 200', value: 'yellow-200' },
+          { title: 'Yellow 100', value: 'yellow-100' },
+          { title: 'Yellow 50', value: 'yellow-50' },
+        ],
+      },
     },
     {
       name: 'heading',
