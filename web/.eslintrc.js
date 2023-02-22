0822f03235b622a43a3284e192ab0bacfa590577
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2023-02-22 15:25:30 +0100

    Add eslint rules

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
+    'plugin:astro/jsx-a11y-strict',
     'plugin:astro/recommended',
     'prettier',
   ],
@@ -17,7 +17,12 @@ module.exports = {
     },
   ],
   rules: {
-    'jsx-a11y/no-redundant-roles': [
+    'astro/no-set-html-directive': 'error',
+    'astro/no-set-text-directive': 'error',
+    'astro/prefer-class-list-directive': 'error',
+    'astro/prefer-object-class-list': 'error',
+    'astro/prefer-split-class-list': 'error',
+    'astro/jsx-a11y/no-redundant-roles': [
       'error',
       {
         ol: ['list'],
